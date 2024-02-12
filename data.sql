5f2bb60cecf11c0d4b5a835b47874048dc004393
MialyNyanj <nyanjatiana@gmail.com>
2024-02-12 13:27:16 +0300

    Initial commit

---
+++ data.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db_P16_etu002371_etu002478_etu002647;
+USE db_P16_etu002371_etu002478_etu002647;
+
+CREATE TABLE utilisateur ();
+CREATE TABLE page_admin ();
+CREATE TABLE variete_the ();
+CREATE TABLE parcelle ();
+CREATE TABLE ctg_depense ();
+CREATE TABLE ();
+CREATE TABLE();
+CREATE TABLE();
