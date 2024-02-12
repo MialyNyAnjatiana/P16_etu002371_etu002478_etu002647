61965c2a62d60d23ab643f9050121668a668fcc8
MialyNyanj <nyanjatiana@gmail.com>
2024-02-12 18:05:07 +0300

    Fonction + img

---
+++ The/traitement-php/user-login.php
@@ -0,0 +1,30 @@
+<?php
+include 'connexion.php';
+
+try {
+    $login = $_POST['name'];
+    $mdp = $_POST['pass'];
+
+    $conn = connect();
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT * FROM utilisateur WHERE nom_user = '$login' AND mdp = '$mdp'";
+    $stmt = $conn->query($sql);
+    $stmt->setFetchMode(PDO::FETCH_OBJ);
+    $user = null;
+
+    while ($row = $stmt->fetch()) {
+        $user = array('id' => $row->id_user, 'name' => $row->nom_user);
+    }
+
+    if ($user['id'] != null) {
+        session_start();
+        $_SESSION['user'] = $user;
+        header('Location:../index.html');
+    } else {
+        header('Location:../admin.html');
+    }
+} catch (PDOException $e) {
+    print 'Erreur :'.$e->getMessage().'<br />';
+    die();
+}
+?>
