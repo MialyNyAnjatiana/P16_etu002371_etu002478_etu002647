ac345a9aef16dfddf36249a53e3b89617b549b60
MialyNyanj <nyanjatiana@gmail.com>
2024-02-13 10:18:17 +0300

    Reload update

---
+++ traitement-php/liste/depense-user-list.php
@@ -0,0 +1,21 @@
+<?php
+include '../connexion.php';
+
+try {
+    $conn = connect();
+
+    $retour = null;
+
+    $result = $conn->query("SELECT * FROM depense AS p JOIN variete_tea AS v ON p.id_var=v.id_var");
+    $result->setFetchMode(PDO::FETCH_OBJ);
+
+    while ($row = $result->fetch()) {
+        $retour[] = array('num' => $row->id_parcelle, 'variete' => $row->nom_tea, 'surface' => $row->surface);
+    }
+
+    echo json_encode($retour);
+} catch (PDOException $e) {
+    print 'Erreur :'.$e->getMessage().'<br />';
+    die();
+}
+?>
